54a8e3082ed32145370ca186ae7cbd4641ffad5d
Aglok <perlovav@gmail.com>
2019-02-27 15:57:03 +0300

    Обновил MTFontManager

--- render/MTFontManager.m
+++ render/MTFontManager.m
@@ -4,14 +4,14 @@
 //
 //  Created by Kostub Deshmukh on 8/30/13.
 //  Copyright (C) 2013 MathChat
-//   
+//
 //  This software may be modified and distributed under the terms of the
 //  MIT license. See the LICENSE file for details.
 //
 
-//#import "MTFontManager.h"
-//#import "MTFont+Internal.h"
-#import "iosMath2-Bridging-Header.h"
+#import "MTFontManager.h"
+#import "MTFont+Internal.h"
+//#import "iosMath2-Bridging-Header.h"
 
 const int kDefaultFontSize = 20;
 
